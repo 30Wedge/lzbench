5658cbe5e5ea06707c8636d0044f6624e35090e5
Andy MacGregor <amacgregor.2018@comcast.net>
2020-03-08 23:50:57 -0400

    supercloud submit scripts

--- job-exclusive-zopfli.sh
+++ job-exclusive-zopfli.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH -J lzbench.%u
-#SBATCH -o result.lzbench.%u.o%j
-#SBATCH -e result.lzbench.%u.e%j
-#SBATCH -n 8
+#SBATCH -J jacobi.%u
+#SBATCH -o jacobi.%u.o%j
+#SBATCH -e jacobi.%u.e%j
+#SBATCH -n 16
 #SBATCH --exclusive
 #SBATCH -p normal # normal queue; use development or serial if queue wait time is too long
 #SBATCH -t 01:00:00 # 1 hour
 
-./lzbench  -p -ezopfli ../enwik8.mini
+./lzbench -ezopfli ../enwik8.mini
